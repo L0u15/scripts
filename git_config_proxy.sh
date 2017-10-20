64b9e9f58afc5d75ab49d5d02b74b6c9a3889222
Louis Lalleau <llalleau@b12s01lnx.elv.enic.fr>
2017-10-20 08:53:17 +0200

    delete old proxy script

--- git_config_proxy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Set univ proxy
-git config --global http.proxy http://cache.univ-lille1.fr:3128
-
-#Save credentials
-git config --global credential.helper cache
-git config --global credential.helper 'cache --timeout=3600'
